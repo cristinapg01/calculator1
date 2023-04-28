88ff791516e5fe61bd6491b1f741ca42aefc503a
cristinapg01 <yqr10540@educastur.es>
2023-04-28 17:23:14 +0000

    Se añaden suma, controlador y test unitario

---
+++ src/main/java/com/ciber1/calculator1/Calculator.java
@@ -0,0 +1,9 @@
+package com.ciber1.calculator1;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+        }
+}
--- src/main/java/com/ciber1/calculator1/Calculator1Application.java
+++ src/main/java/com/ciber1/calculator1/Calculator1Application.java
@@ -11,3 +11,4 @@ public class Calculator1Application {
 	}
 
 }
+
